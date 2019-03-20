efec172e1264d2d3b15513c47e0af30fb30e4cd8
Pongsakorn Onsri <pongsakorn@central.tech>
2019-03-20 15:03:53 +0700

    change auto reply to echo bot

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const request = require('request-promise');
+
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
+const LINE_HEADER = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer Cd9zDe9LZvQ5UDfVZZElDTY+0lfW26UOZ210/QMozN4o8jXjGt9ckRCCUYFv+/bZO9paIZL7b23WA2lxS+OYknIM26TYrkUxwK2Kp8/1nXPsKwkafkX2jckSggjhC9FLzA1Hio3glJsCwIM5GdeXRQdB04t89/1O/w1cDnyilFU=`
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.LineBot = functions.https.onRequest((req, res) => {
-  res.send('Hello World!');
+  if (req.body.events[0].message.type !== 'text') {
+    return;
+  }
+  reply(req.body);
 });
+
+const reply = (bodyResponse) => {
+  return request({
+    method: `POST`,
+    uri: `${LINE_MESSAGING_API}/reply`,
+    headers: LINE_HEADER,
+    body: JSON.stringify({
+      replyToken: bodyResponse.events[0].replyToken,
+      messages: [
+        {
+          type: `text`,
+          text: bodyResponse.events[0].message.text
+        }
+	  ]
+    })
+  });
+};
