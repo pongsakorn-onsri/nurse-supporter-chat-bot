a600f918debb302a1cc6effec4e504fc37d24964
Pongsakorn Onsri <pongsakorn@central.tech>
2019-03-20 17:33:32 +0700

    chat return all types

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
-const request = require('request-promise');
+const functions = require("firebase-functions");
+const request = require("request-promise");
 
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
 const LINE_HEADER = {
@@ -11,25 +11,24 @@ const LINE_HEADER = {
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.LineBot = functions.https.onRequest((req, res) => {
-  if (req.body.events[0].message.type !== 'text') {
-    return;
+  if (req.method === "POST"){
+    reply(req.body);
+  } else {
+    return res.status(200).end();
   }
-  reply(req.body);
 });
 
-const reply = (bodyResponse) => {
+const reply = bodyResponse => {
   return request({
     method: `POST`,
     uri: `${LINE_MESSAGING_API}/reply`,
     headers: LINE_HEADER,
     body: JSON.stringify({
       replyToken: bodyResponse.events[0].replyToken,
-      messages: [
-        {
-          type: `text`,
-          text: bodyResponse.events[0].message.text
-        }
-	  ]
+      messages: [{
+        type: `text`,
+        text: JSON.stringify(bodyResponse)
+      }]
     })
   });
 };
